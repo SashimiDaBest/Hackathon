bac9421839d5169cd70ae4b5a5841435e47a1952
Sashimi <86940811+SashimiDaBest@users.noreply.github.com>
2022-05-29 14:53:36 +0000

    Using classes

--- MainPage/Home.java
+++ MainPage/Home.java
@@ -1,33 +1,10 @@
 import java.util.*;
 //simple formula by https://justenergy.com/blog/how-to-calculate-your-carbon-footprint/
 public class Home{ //Calculate and mkke double array from user's input to make pie chart
-  public static void main(String[] args){
-    Scanner in = new Scanner(System.in);
-    System.out.println("User Input (MONTHLY)");
-
-    String[] aspect = {"electric bill: ", "gas bill: ", "oil bill: ", "car millage: ", "number of flights in hours: ", "recycle paper(true/false)? ", "recycle aluminum and tin(true/false)? "};
-
-    boolean[] two = new boolean[2];
-    double[] five = new double[5];
-    double[] list = new double[7];
-          
-    for(int i = 0; i < aspect.length; i++){
-      if(i >= 5){
-        System.out.print(aspect[i]);
-        boolean ans = in.nextBoolean();
-        two[i-5] = ans;
-      }
-      else{
-        System.out.print(aspect[i]);
-        double value = in.nextDouble();
-        five[i] = value;
-      }
-    }
-    createList(list, five, two);
-    formatReturn(list);
-
-  }
-  static double[] createList(double[] list, double[] arr2, boolean[] arr3){
+  public double[] list = new double[7];
+  private String[] aspect = {"electric bill: ", "gas bill: ", "oil bill: ", "car millage: ", "number of flights in hours: ", "recycle paper(true/false)? ", "recycle aluminum and tin(true/false)? "};
+  
+  public double[] createList(double[] arr2, boolean[] arr3){
     for(int i = 0; i < arr2.length; i++){
       if(i == 0 || i == 1){
         list[i] = i * 105;
@@ -47,23 +24,19 @@ public class Home{ //Calculate and mkke double array from user's input to make p
     } 
     for(int i = 0; i < arr3.length; i++){
       if(i == 0){
-        list[3] = i + 184;
+        list[5] = 184;
       }
       else{
-        list[4] = i + 166;
+        list[6] = 166;
       }
     }
     return list;
   }
-  static void formatReturn(double[] list){
-    double total = 0;
-    for(double x:list){
-      total += x;
-    }
-    System.out.println("User Annual Carbon Footprint: "+total);
-  }
 
-  static double[] getArr(){
+  public double[] getArr(){
     return list;
+  }
+  public String[] getAspect(){
+    return aspect;
   }
 }
--- MainPage/Progress.java
+++ MainPage/Progress.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 public class Progress{ 
+  public static int goalSet(double[] arr){
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("How much carbon reduction? ");
+    int diff = in.nextInt();
+    
+    int total = 0;
+    for(double x: arr){
+      total += x;
+    }
+    return (int)total - diff;
+  }
   public static void main(String[] args){
     double[] list = Home.getArr();
   }
